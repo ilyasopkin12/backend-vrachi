8b6d86fdff3d86c75fb64a81bb8935a34edb1865
Ilya <steamistl@gmail.com>
2026-04-06 21:46:23 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { HttpExceptionFilter } from './common/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:4000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -28,4 +33,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
